40d8bdd74a0d6d7469b139548c5faebc8bf3b0a2
J. David Lowe <j.david.lowe@gmail.com>
2012-08-28 16:56:46 -0700

    major state transitions...

--- death.c
+++ death.c
@@ -6,18 +6,37 @@ int main(void) {
     Window window    = XCreateSimpleWindow(display, RootWindow(display, screen),
         40, 40, 1024, 512, 3, BlackPixel(display, screen), WhitePixel(display, screen));
 
-    XSelectInput(display, window, ExposureMask | KeyPressMask);
+    enum { unmapped, splash, quit, playing, dead } state = unmapped;
+
+    XSelectInput(display, window, ExposureMask | KeyPressMask | StructureNotifyMask);
     XMapWindow(display, window);
 
-    while (1) {
+    while (state != quit) {
         XEvent event;
-
         XNextEvent(display, &event);
-        if (event.type == Expose) {
-            /* initial draw */
-        }
-        if (event.type == KeyPress)
-            break;
+
+        switch (state) {
+            case unmapped:
+                if (event.type == MapNotify) {
+                    state = splash;
+                }
+                break;
+            case splash:
+                switch (event.type) {
+                    case Expose:
+                        /* initial draw */
+                        break;
+                    case KeyPress:
+                        state = quit;
+                        break;
+                };
+            case playing:
+                break;
+            case dead:
+                break;
+            case quit:
+                break;
+        };
     }
 
     XCloseDisplay(display);
